c90fbd666f0f6a8a20271e495b76b8064ddaf0ea
SHIJAS TK <107620597+Shijastk@users.noreply.github.com>
2026-09-25 17:18:57 +0530

    test: remove unused Flutter import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:solar_ops_mobile/api.dart';
 import 'package:solar_ops_mobile/app_controller.dart';
